297304998692653a5e98ad5eb11719a002f69533
Stuart Jones <Me@Stuart-Jones.com>
2018-02-21 09:22:48 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# It's Sharkle!
+
+## What does this app do?
+
+It's Sharkle!
+
+## No really, what does it do?
+
+It's Sharkle is a small Mac applet that puts the Sharkle desktop toy from the indie game Night in the Woods on your desktop. If your click it, he says Hey~! You can also drag Shakle around and put him wherever you'd like.
+
+## Can it do anything else?
+
+If you go to the boat icon on your taskbar, you can quit the app, invert Shakle's colors, or make him face the other direction.
+
+## Why is this app so large?
+
+I'm a web developer, not a desktop app developer. Shakle's written in a large framework called Electron, which needs to be included in your app distribution.
+
+## How do I build Shakle myself?
+
+`npm install` and `npm start`. Shakle needs Electron 1.8.2.
+
+## Where's the Windows version?
+
+Unfortunately there's some strangeness around Electron and transparent frameless windows on Windows. If you get it working, feel free to let me know.
---
+++ main.js
@@ -0,0 +1,85 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const Tray = electron.Tray
+const Menu = electron.Menu
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 318, height: 306, skipTaskbar: true, frame: false, transparent: true})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createWindow()
+  // Hide dock icon.
+  if(process.platform === 'darwin') {
+    app.dock.hide();
+  }
+
+  tray = new Tray(path.join(__dirname, './icons/baricon.png'))
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Invert Colors', type: 'checkbox', checked: false, click(item) {
+        console.log("Hey you clicked invert! " + item.checked);
+        mainWindow.webContents.executeJavaScript('invertColors()');
+    }},
+    {label: 'Flip Direction', type: 'checkbox', click(item) {
+        console.log("Hey you clicked flip! " + item.checked);
+        mainWindow.webContents.executeJavaScript('flipDirection()');
+    }},
+    {label: 'Quit', click () { app.quit(); }}
+  ])
+  tray.setToolTip('Hey~!')
+  tray.setContextMenu(contextMenu)
+})
+
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
